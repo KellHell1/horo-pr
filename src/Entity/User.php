93cad2936f1cddcdf2994314abe83f293ec5af31
Sakhno Mykola <m.sakhno@sanmanto.com>
2024-11-08 01:48:57 +0200

    create user

---
+++ src/Entity/User.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'users')]
+#[ORM\UniqueConstraint(columns: ['login', 'pass'])]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 8)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 8)]
+    private string $login;
+
+    #[ORM\Column(type: 'string', length: 8)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 8)]
+    private string $phone;
+
+    #[ORM\Column(type: 'string', length: 8)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 8)]
+    private string $pass;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+        return $this;
+    }
+
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    public function getPass(): string
+    {
+        return $this->pass;
+    }
+
+    public function setPass(string $pass): self
+    {
+        $this->pass = $pass;
+        return $this;
+    }
+}
